1e270d7e6be5e47dd8b6891a1ddea579e9c29566
Aller Augusto de Souza <allerasouza@gmail.com>
2022-08-06 15:27:30 -0300

    chore: setup flyway migrations

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -22,6 +22,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-mysql")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
