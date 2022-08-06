af2c1c220b9b204fc9aa1b309862f32ff1a33691
Aller Augusto de Souza <allerasouza@gmail.com>
2022-08-06 20:07:28 -0300

    feat: add other crud methods to book service

--- mercado-livro/src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.BookStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.repository.BookRepository
 import org.springframework.stereotype.Service
@@ -10,6 +11,28 @@ class BookService(
 ) {
 
     fun create(book: BookModel) {
+        bookRepository.save(book)
+    }
+
+    fun findAll(): List<BookModel> {
+        return bookRepository.findAll().toList()
+    }
+
+    fun findActives(): List<BookModel> {
+        return bookRepository.findByStatus(BookStatus.ACTIVE)
+    }
+
+    fun findById(id: Int): BookModel {
+        return bookRepository.findById(id).orElseThrow()
+    }
+
+    fun delete(id: Int) {
+        val book = findById(id)
+        book.status = BookStatus.CANCELED
+        update(book)
+    }
+
+    fun update(book: BookModel) {
         bookRepository.save(book)
     }
 }
