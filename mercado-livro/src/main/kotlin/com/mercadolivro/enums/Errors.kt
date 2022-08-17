0e7cc9878e169055c0415de3bfecb2cdc263a3a3
Aller Augusto de Souza <allerasouza@gmail.com>
2022-08-17 09:16:19 -0300

    feat: add bad request exception

--- mercado-livro/src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -1,6 +1,12 @@
 package com.mercadolivro.enums
 
 enum class Errors(val code: String, val message: String) {
+
+    // Book
     ML101("ML-101", "Book [%s] not exists"),
+    ML102("ML-102", "Cannot update book with status [%s]"),
+
+    // Customer
     ML201("ML-201", "Customer [%s] not exists")
+
 }
